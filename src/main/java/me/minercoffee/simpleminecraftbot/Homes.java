dbdaeea951124cc8ab9c1014eebf7b47fa0c031c
MinerCoffee <realcoffeeminer@gmail.com>
2022-09-23 11:31:18 -0700

    getting closer with the right results for creating homes without removing someone else home with a similar name.

--- src/main/java/me/minercoffee/simpleminecraftbot/Homes.java
+++ src/main/java/me/minercoffee/simpleminecraftbot/Homes.java
@@ -4,7 +4,6 @@ import me.minercoffee.simpleminecraftbot.utils.ColorMsg;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -39,24 +38,24 @@ public class Homes implements TabExecutor {
                     }
                     homeName = builder.toString();
                     homeName = homeName.stripTrailing();
-                     if (args.length == 1 && args[0].equalsIgnoreCase("return")) {
-                         if (getDataConfig().isConfigurationSection("staff-homes-locations." + p.getName() + homeName)) {
+/*                     if (args.length == 1 && args[0].equalsIgnoreCase("return")) { //teleports owner to homeName
+                         if (getDataConfig().isConfigurationSection("staff-homes-locations." +  homeName)) {
                              System.out.println("test");
-                             Location return_location = new Location(p.getWorld(), getDataConfig().getInt("staff-homes-locations." + p.getName() + homeName  + ".x"), //TODO Added Pinch & YAW & World. Ex: world_the_nether
-                                     getDataConfig().getInt("staff-homes-locations." + p.getName() + homeName  + ".y")
-                                     , getDataConfig().getInt("staff-homes-locations." + p.getName() + homeName + ".z"));
+                             Location return_location = new Location(p.getWorld(), getDataConfig().getInt("staff-homes-locations." + homeName + ".x"), //TODO Added Pinch & YAW & World. Ex: world_the_nether
+                                     getDataConfig().getInt("staff-homes-locations." +  homeName  + ".y")
+                                     , getDataConfig().getInt("staff-homes-locations." +  homeName + ".z"));
                              p.teleport(return_location);
                              p.sendMessage(ColorMsg.color(getMessagesConfig().getString("return-message")));
                          } else {
                              p.sendMessage(ChatColor.DARK_RED + "You never set a staff home.");
                          }
-                     }
+                    } */
 
                     if (args.length == 1 && args[0].equalsIgnoreCase("help")) {
                         p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7==&a&lStaff&eHomes&7 by MinerCoffee=="));
                         p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6&o/staffhome set <name> &7- &9Set a Temporary Home"));
                         p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6&o/staffhome return &7- &9Return to Home"));
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6&o/staffhome delete &7- &9Removes your home"));
+                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6&o/staffhome delete homeName &7- &9Removes your home"));
                         p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7========================="));
                         return true;
                     }
@@ -67,14 +66,20 @@ public class Homes implements TabExecutor {
                         p.sendMessage(ColorMsg.color("&cPlease provide a name. Ex: /staffhome set <name>"));
                         return true;
                     }
+
+                    if (args.length == 1 && args[0].equalsIgnoreCase("delete")) {
+                        String HomeName = getDataConfig().getString("staff-homes-locations." + homeName + ".homename");
+                        p.sendMessage(ColorMsg.color("&cPlease provide a your" + " " +  HomeName + " " + "with the homeName you created."));
+                        return true;
+                    }
                    homeName = args[1];
                     if (args.length == 2 && args[0].equalsIgnoreCase("set")) {
-                        if (getDataConfig().isConfigurationSection("staff-homes-locations." + p.getName() + homeName)) {
+                        if (getDataConfig().isConfigurationSection("staff-homes-locations." +  homeName)) {
                             p.sendMessage(ColorMsg.color(getMessagesConfig().getString("override-message")) +
-                                    getDataConfig().getInt("staff-homes-locations." + p.getName() + homeName + ".x") + " " +
-                                    getDataConfig().getInt("staff-homes-locations." + p.getName() + homeName + ".y")
-                                    + " " + getDataConfig().getInt("staff-homes-locations." + p.getName() + homeName + ".z") + " " + getDataConfig().getInt("staff-homes-locations." + p.getName() + homeName + ".pinch") + " " +
-                                    getDataConfig().getInt("staff-homes-locations." + p.getName() + homeName + ".yaw") + " " +  getDataConfig().getLocation("staff-homes-locations." + p.getName() + homeName + ".world")); //TODO Added Pinch & YAW & World. Ex: world_the_nether
+                                    getDataConfig().getInt("staff-homes-locations." +  homeName + ".x") + " " +
+                                    getDataConfig().getInt("staff-homes-locations." +  homeName + ".y")
+                                    + " " + getDataConfig().getInt("staff-homes-locations." +  homeName  +".z") + " " + getDataConfig().getInt("staff-homes-locations." +  homeName +  ".pinch") + " " +
+                                    getDataConfig().getInt("staff-homes-locations." +  homeName + ".yaw")); //TODO Added Pinch & YAW & World. Ex: world_the_nether
                             saveLocation(p);
                         } else {
                             saveLocation(p);
@@ -83,27 +88,36 @@ public class Homes implements TabExecutor {
                     if (args.length == 3 && args[0].equalsIgnoreCase("return")) { //staffhome return <target> homeName
                         if (p.hasPermission("simpleminecraftbot.staff") || p.isOp()) {
                             Player t = Bukkit.getPlayer(args[1]);
-                            if (!(t == null)) {
-                                homeName = args[2];
-                                if (getDataConfig().isConfigurationSection("staff-homes-locations." + t.getName() + homeName)) {
-                                    p.sendMessage(ChatColor.GREEN + "Teleporting to temporary staff home(" + t.getName() + homeName + ") @: " + ChatColor.GRAY + getDataConfig().getInt("staff-homes-locations." + t.getName() + homeName + ".x") + " " + getDataConfig().getInt("staff-homes-locations." + t.getName() + homeName + ".y") + " " + getDataConfig().getInt("staff-homes-locations." + t.getName() + homeName + ".z"));
-                                    Location return_location = new Location(t.getWorld(), getDataConfig().getInt("staff-homes-locations." + t.getName() + homeName + ".x"), getDataConfig().getInt("staff-homes-locations." + t.getName() + homeName + ".y"), getDataConfig().getInt("staff-homes-locations." + t.getName() + homeName + ".z")); //TODO Added Pinch & YAW & World. Ex: world_the_nether
-                                    p.teleport(return_location);
+                            String worldName = getDataConfig().getString("staff-homes-locations." + homeName + ".worldName");
+                            String HomeName = getDataConfig().getString("staff-homes-locations." + homeName + ".homename");
+                                homeName = args[2]; //ToDo fix msg it shows args instead of the homeName
+                                if (getDataConfig().isConfigurationSection("staff-homes-locations." + homeName )) {
+                                    p.sendMessage(ChatColor.GREEN + "Teleporting to " + HomeName + " in the" + worldName + " " + "@: " + ChatColor.GRAY + getDataConfig().getInt("staff-homes-locations." +  homeName + ".x") + " " + getDataConfig().getInt("staff-homes-locations." + homeName + ".y") + " " + getDataConfig().getInt("staff-homes-locations." + homeName + ".z"));
+                                    Location return_location = null; //TODO Added Pinch & YAW & World. Ex: world_the_nether
+                                    if (t != null) {
+                                        return_location = new Location(t.getWorld(), getDataConfig().getInt("staff-homes-locations." + homeName + ".x"), getDataConfig().getInt("staff-homes-locations." + homeName + ".y"), getDataConfig().getInt("staff-homes-locations." + homeName + ".z"));
+                                    }
+                                    if (return_location != null) {
+                                        p.teleport(return_location);
+                                    }
                                 } else {
                                     p.sendMessage(ChatColor.DARK_RED + "That player does not have a home set.");
                                 }
-
-                            }
                         } else {
                             p.sendMessage(ChatColor.DARK_RED + "You don't have permission to use this command.");
                         }
                     }
-                    if (args.length == 2 && args[0].equalsIgnoreCase("delete")) {
+                    if (args.length == 2 && args[0].equalsIgnoreCase("delete")) { //staffhome delete playerNamehomeNAme
                         homeName = args[1];
+                        String HomeName = getDataConfig().getString("staff-homes-locations." + homeName + ".homename");
+                        String PlayerName = getDataConfig().getString("staff-homes-locations." + homeName + ".owner");
                         if (p.hasPermission("simpleminecraftbot.staff") || p.isOp()) {
-                            getDataConfig().set("staff-homes-locations." + p.getName() + homeName, null); //TODO make it delete the home with the owner with it. Essentially it would find the name of the home and then find the owner lastly delete the home
-                            SaveData();
-                            p.sendMessage(ColorMsg.color("&c&lStaff Home Deleted!"));
+                            if (getDataConfig().isConfigurationSection("staff-homes-locations." + homeName)) {
+                                    System.out.println("testing delete");
+                                    getDataConfig().set("staff-homes-locations." + homeName, null); //TODO make it delete the home with the owner with it. Essentially it would find the name of the home and then find the owner lastly delete the home
+                                    SaveData();
+                                    p.sendMessage(ColorMsg.color("&c&lStaff Home Deleted!"));
+                            }
                         }
                     }
                 }
@@ -113,16 +127,17 @@ public class Homes implements TabExecutor {
     }
     private void saveLocation(@NotNull Player p) {
         Location l = p.getLocation();
+        //TODO Make Variables for the cords
         p.sendMessage(ChatColor.translateAlternateColorCodes('&', getMessagesConfig().getString("set-message") + Math.round(l.getX()) + " " + Math.round(l.getY()) + " " + Math.round(l.getZ())));
-        getDataConfig().createSection("staff-homes-locations." + p.getName() + homeName);
-        getDataConfig().set("staff-homes-locations." + p.getName() + homeName + ".homename", homeName);
-        getDataConfig().set("staff-homes-locations." + p.getName() + homeName + ".owner", p.getName());
-        getDataConfig().set("staff-homes-locations." + p.getName() + homeName + ".x", l.getX());
-        getDataConfig().set("staff-homes-locations." + p.getName() + homeName + ".y", l.getY());
-        getDataConfig().set("staff-homes-locations." + p.getName() + homeName + ".z", l.getZ());
-        getDataConfig().set("staff-homes-locations." + p.getName() + homeName + ".yaw", l.getYaw());
-        getDataConfig().set("staff-homes-locations." + p.getName() + homeName + ".pitch", l.getPitch());
-        getDataConfig().set("staff-homes-locations." + p.getName() + homeName + ".worldtype", l.getWorld());
+        getDataConfig().createSection("staff-homes-locations." + homeName);
+        getDataConfig().set("staff-homes-locations." +  homeName + ".homename", homeName);
+        getDataConfig().set("staff-homes-locations." +  homeName + ".owner",  p.getName());
+        getDataConfig().set("staff-homes-locations." +  homeName +  ".x", l.getX());
+        getDataConfig().set("staff-homes-locations." +  homeName +  ".y", l.getY());
+        getDataConfig().set("staff-homes-locations." +  homeName +  ".z", l.getZ());
+        getDataConfig().set("staff-homes-locations." +  homeName +  ".yaw", l.getYaw());
+        getDataConfig().set("staff-homes-locations." +  homeName +  ".pitch", l.getPitch());
+        getDataConfig().set("staff-homes-locations." +  homeName +  ".worldName", l.getWorld().getName());
         SaveData();
     }
 
